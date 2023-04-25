9fe5f7c3021acc08b5dad258b7e27b7266d52d90
sejili <sejingun06@naver.com>
2023-04-25 18:41:19 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int times = 1 * 60;
+
+  void timeStart() {
+    Timer timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        print(times--);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(children: [
+          Flexible(
+              flex: 1,
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.black,
+                child: const Center(
+                  child: Text(
+                    'My timer',
+                    style: TextStyle(color: Colors.white, fontSize: 50),
+                  ),
+                ),
+              )),
+          Flexible(
+              flex: 3,
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.blue,
+                child: Center(
+                  child: Text(
+                    '$times',
+                    style: const TextStyle(fontSize: 50),
+                  ),
+                ),
+              )),
+          Flexible(
+              flex: 1,
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: const Color.fromARGB(255, 1, 40, 71),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: timeStart,
+                        icon: const Icon(Icons.play_arrow_rounded),
+                        iconSize: 50,
+                      ),
+                      const IconButton(
+                        onPressed: null,
+                        icon: Icon(Icons.play_circle_rounded),
+                        iconSize: 50,
+                      )
+                    ],
+                  ),
+                ),
+              )),
+        ]),
+      ),
+    );
+  }
+}
